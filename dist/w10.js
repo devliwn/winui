a8c8c781f541cc285f1698158ed226b8bd120478
devliwn <hainguyen.pro09@gmail.com>
2023-01-07 13:07:52 +0700

    comment

--- dist/w10.js
+++ dist/w10.js
@@ -19,85 +19,82 @@ document.head.insertAdjacentHTML(
 const body = document.querySelector("#i");
 function zink(code) {
     var data = code.split("\n").filter(String);
-    // console.log(data);
     data.forEach((line) => {
-        // console.log(line);
-        var keyWord = line.split("~").filter(String)[0];
-        var property = line.split("~").filter(String)[1].replace("{", "").replace("}", "");
-        // console.log(keyWord);
-        switch (keyWord) {
-            // ========== SCREEN SETUP ==========
-            case "scr": {
-                // console.log(property);
-                var sizes = property.split(",");
-                console.log(sizes);
-                body.style.width = sizes[0] + "px";
-                body.style.height = sizes[1] + "px";
-                break;
-            }
-            case "scrBg": {
-                body.style.backgroundColor = property;
-                break;
-            }
-            // ========== DRAW ==========
-            case "draw:rect": {
-                var styles = property.split("|");
-                var position = styles[0].split(",");
-                var sizes = styles[1].split(",");
-                var color = styles[2];
-                var border = styles[3];
-                var parameter = `${position[0]},${position[1]} ${position[0]},${parseInt(sizes[1]) + parseInt(position[1])} ${parseInt(sizes[0]) + parseInt(position[0])},${parseInt(sizes[1]) + parseInt(position[1])} ${parseInt(sizes[0]) + parseInt(position[0])},${position[1]}`;
-                function checkBorder() {
-                    if (border == undefined) {
-                        return "";
-                    } else {
-                        return border.split(",");
-                    }
+        // ========== COMMENT ==========
+        if (line[0] == "#") {
+            return "";
+        } else {
+            var keyWord = line.split("~").filter(String)[0];
+            var property = line.split("~").filter(String)[1].replace("{", "").replace("}", "");
+            switch (keyWord) {
+                // ========== SCREEN SETUP ==========
+                case "scr": {
+                    var sizes = property.split(",");
+                    console.log(sizes);
+                    body.style.width = sizes[0] + "px";
+                    body.style.height = sizes[1] + "px";
+                    break;
                 }
-                body.insertAdjacentHTML(
-                    "beforeend",
-                    `
-                    <polygon
-                    points="${parameter}"
-                    style="fill:${color}; stroke:${checkBorder()[1]}; stroke-width:${checkBorder()[0]}"
-                    >
-                    `
-                );
-                break;
-            }
-            case "draw:tri": {
-                var styles = property.split("|");
-                var a = styles[0];
-                var b = styles[1];
-                var c = styles[2];
-                var color = styles[3];
-                var border = styles[4];
-                function checkBorder() {
-                    if (border == undefined) {
-                        return "";
-                    } else {
-                        return border.split(",");
-                    }
+                case "scrBg": {
+                    body.style.backgroundColor = property;
+                    break;
                 }
-                var parameter = `${a} ${b} ${c}`;
-                console.log(parameter);
-                body.insertAdjacentHTML(
-                    "beforeend",
-                    `
-                    <polygon
-                    points="${parameter}"
-                    style="fill:${color}; stroke:${checkBorder()[1]}; stroke-width:${checkBorder()[0]}"
-                    >
-                    `
-                );
-            }
-            // ========== COMMENT ==========
-            case "?": {
-                break;
-            }
-            // ========== ERROR ==========
-            default: {
-                body.insertAdjacentHTML("beforeend", `<p style='color: red'>The keyWord you typed ('${keyWord}') is not supported.</p>`);
+                // ========== DRAW ==========
+                case "draw:rect": {
+                    var styles = property.split("|");
+                    var position = styles[0].split(",");
+                    var sizes = styles[1].split(",");
+                    var color = styles[2];
+                    var border = styles[3];
+                    var parameter = `${position[0]},${position[1]} ${position[0]},${parseInt(sizes[1]) + parseInt(position[1])} ${parseInt(sizes[0]) + parseInt(position[0])},${parseInt(sizes[1]) + parseInt(position[1])} ${parseInt(sizes[0]) + parseInt(position[0])},${position[1]}`;
+                    function checkBorder() {
+                        if (border == undefined) {
+                            return "";
+                        } else {
+                            return border.split(",");
+                        }
+                    }
+                    body.insertAdjacentHTML(
+                        "beforeend",
+                        `
+                        <polygon
+                        points="${parameter}"
+                        style="fill:${color}; stroke:${checkBorder()[1]}; stroke-width:${checkBorder()[0]}"
+                        >
+                        `
+                    );
+                    break;
+                }
+                case "draw:tri": {
+                    var styles = property.split("|");
+                    var a = styles[0];
+                    var b = styles[1];
+                    var c = styles[2];
+                    var color = styles[3];
+                    var border = styles[4];
+                    function checkBorder() {
+                        if (border == undefined) {
+                            return "";
+                        } else {
+                            return border.split(",");
+                        }
+                    }
+                    var parameter = `${a} ${b} ${c}`;
+                    console.log(parameter);
+                    body.insertAdjacentHTML(
+                        "beforeend",
+                        `
+                        <polygon
+                        points="${parameter}"
+                        style="fill:${color}; stroke:${checkBorder()[1]}; stroke-width:${checkBorder()[0]}"
+                        >
+                        `
+                    );
+                }
+                // ========== ERROR ==========
+                default: {
+                    body.insertAdjacentHTML("beforeend", `<p style='color: red'>The keyWord you typed ('${keyWord}') is not supported.</p>`);
+                }
             }
         }
     });
@@ -105,7 +102,7 @@ function zink(code) {
 zink(`
 scr~{1000,500}
 scrBg~{#333}
-?~{=====DRAW=====}
+#=====DRAW=====
 draw:rect~{20,20|300,150|transparent|1,#fff}
 draw:tri~{170,20|20,170|320,95|red|1,#fff}
 `);
